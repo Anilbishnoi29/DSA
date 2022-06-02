edc1333bb58f1e654944d4641d4d2aa419d14019
Anilbishnoi29 <bishnoianilakb@gmail.com>
2022-06-02 16:31:05 +0530

    L3

---
+++ CodeHelp/Lecture_03/Q01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Enter something";
+    char ch;
+    cin >> ch;
+
+    if (ch >= 'a' && ch <= 'z') {
+        cout << "This Is Lowercase";
+    }
+    else if (ch >= 'A' && ch <= 'Z') {
+        cout << "This is Uppercase";
+
+    }
+    else {
+        cout << "This is numeric";
+    }
+
+    return 0;
+}
